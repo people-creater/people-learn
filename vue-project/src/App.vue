0f8ef7834fd943ec0627022dfedd07862d8a4af6
people <540635325@qq.com>
2025-09-23 15:00:57 +0800

    初始化推送

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="App">
+    <Header :title="MyTitle" @changeTitle="handleTitle"></Header>
+    <Main></Main>
+    <Footer></Footer>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+export default {
+  // 声明组件
+  components: {Main, Header,Footer},
+  data() {
+    return {
+      MyTitle: '这是标题'
+    }
+  },
+  methods: {
+    handleTitle(newTitle){
+      this.MyTitle = newTitle
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+</script>
+<style>
+.App{
+  width: auto;
+  height: 600px;
+  background-color: pink;
+  line-height: 600px;
+  text-align: center;
+  font-size: 20px;
 }
 </style>
---
+++ vue-project/src/components/Main.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="main"></div>
+</template>
+
+<style scoped>
+.main{
+  width: 100%;
+  height: 300px;
+  line-height: 300px;
+  text-align: center;
+  background-color: #535bf2;
+  color: white;
+}
+</style>
