abc925bcbe4f554db7d39a3526c8d8ead4ee39f3
people <540635325@qq.com>
2025-10-16 09:49:12 +0800

    初始化推送

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -32,6 +32,123 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   }
+
+  function resetPreviewImage(){
+    menuPreviewImg.innerHTML = '';
+    const defaultImg = document.createElement('img');
+    defaultImg.src = '../src/assets/nature-1920.jpg';
+    menuPreviewImg.appendChild(defaultImg);
+  }
+
+  function animateMenuToggle(isOpening){
+    const open = document.querySelector('p#menu-open');
+    const close = document.querySelector('p#menu-close');
+
+    gsap.to(isOpening ? open : close ,{
+      x: isOpening ? -5 : 5,
+      y: isOpening ? -10 : 10,
+      rotation: isOpening ? -5 : 5,
+      opacity: 0 ,
+      delay : 0.25,
+      ease: "power2.out",
+    })
+
+    gsap.to(isOpening ? close : open ,{
+      x: 0,
+      y: 0,
+      rotation: 0,
+      opacity: 1 ,
+      delay : 0.5,
+      duration: 0.5,
+      ease: "power2.out",
+    })
+  }
+
+  function openMenu(){
+    if(isAnimating || isOpen) return;
+    isAnimating = true;
+
+    gsap.to(container,{
+      rotation: 10,
+      x: 300,
+      y: 450,
+      scale: 1.5,
+      duration: 1.25,
+      ease: "power4.inOut",
+    })
+
+    animateMenuToggle(true);
+
+    gsap.to(menuContent,{
+      rotation: 0,
+      x: 0,
+      y: 0,
+      scale: 1,
+      opacity: 1,
+      duration: 1.25,
+      ease: "power4.inOut",
+    })
+
+    gsap.to([".link a",".social a"],{
+      y: "0%",
+      opacity: 1,
+      duration: 1,
+      delay: 0.75,
+      stagger: 0.1,
+      ease: "power3.out",
+    })
+
+    gsap.to(menuOverlay,{
+      clipPath:"polygon:0% 0%,100% 0%,100% 175%,0% 100%)",
+      duration: 1.25,
+      ease: "power4.inOut",
+      onComplete: () => {
+        isAnimating = false;
+        isOpen = true;
+      }
+    })
+  }
+
+  function closeMenu(){
+    if(isAnimating || !isOpen) return;
+    isAnimating = true;
+
+    gsap.to(container,{
+      rotation: 0,
+      x: 0,
+      y: 0,
+      scale: 1,
+      duration: 1.25,
+      ease: "power4.inOut",
+    })
+
+    animateMenuToggle(false);
+
+    gsap.to(menuContent,{
+      rotation: -15,
+      x: -100,
+      y: -100,
+      scale: 1.5,
+      opacity: 0.25,
+      duration: 1.25,
+      ease: "power4.inOut",
+    })
+
+    gsap.to(menuOverlay,{
+      clipPath:"polygon:0% 0%,100% 0%,100% 0%,0% 0%)",
+      duration: 1.25,
+      ease: "power4.inOut",
+      onComplete: () => {
+        isOpen = false;
+        isAnimating = false;
+        gsap.set([".link a",".social a"],{
+          y:120%,
+        })
+        resetPreviewImage();
+      }
+    })
+  }
+
 })
 
 </script>
